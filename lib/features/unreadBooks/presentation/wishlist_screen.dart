cd1feccfcbb63f867ff94189d10de8f7aad4f3f2
Queengirl1990 <bruehlanja90@gmail.com>
2024-02-05 14:43:58 +0100

    wishlist design

--- lib/features/unreadBooks/presentation/wishlist_screen.dart
+++ lib/features/unreadBooks/presentation/wishlist_screen.dart
@@ -1,24 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import '../../../backend/styles/appbar.dart';
 import '../../../backend/styles/theme.dart';
-import '../../../backend/widgets/bookieslist_widgets.dart';
 import '../../../backend/libary/unread_books_datenbank.dart';
-
-//liste Teilen funktion einbauen
+import '../../home/presentation/home_screen.dart';
 
 void main() {
   runApp(const WishListApp());
 }
 
 class WishListApp extends StatelessWidget {
-  const WishListApp({super.key});
+  const WishListApp({Key? key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Wish List App',
       theme: ThemeData(
-        backgroundColor: CustomTheme.darkRed,
+        colorScheme: ColorScheme.fromSwatch(
+          backgroundColor: CustomTheme.darkRed,
+        ),
       ),
       home: const WishListScreen(),
     );
@@ -26,7 +28,7 @@ class WishListApp extends StatelessWidget {
 }
 
 class WishListScreen extends StatefulWidget {
-  const WishListScreen({super.key});
+  const WishListScreen({Key? key});
 
   @override
   _WishListScreenState createState() => _WishListScreenState();
@@ -34,100 +36,166 @@ class WishListScreen extends StatefulWidget {
 
 class _WishListScreenState extends State<WishListScreen> {
   void addNewBook() {
-    // buch hinzufügen
+    // Buch hinzufügen
+  }
+
+  bool _showShimmer = true;
+
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(milliseconds: 1000), () {
+      if (mounted) {
+        setState(() {
+          _showShimmer = false;
+        });
+      }
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          "Wunschliste",
-          style: TextStyle(
-            fontFamily: 'DancingScript',
-            fontWeight: FontWeight.bold,
-            fontSize: 24,
-            color: CustomTheme.snowWhite,
-          ),
-        ),
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back),
-          color: CustomTheme.snowWhite,
-          onPressed: () {
-            // pfad
-          },
-        ),
-        actions: [
-          myCircularAvatar(),
-          const SizedBox(width: 16),
-        ],
-        backgroundColor: CustomTheme.darkRed,
-        elevation: 0,
-      ),
-      backgroundColor: CustomTheme.darkRed,
-      body: GridView.builder(
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 3,
-          childAspectRatio: 80 / 100,
-        ),
-        itemCount: wishList.length + 1,
-        itemBuilder: (context, index) {
-          if (index == 0) {
-            return GestureDetector(
-              onTap: addNewBook,
-              child: Container(
-                margin: const EdgeInsets.all(8),
-                decoration: BoxDecoration(
-                  color: CustomTheme.darkRed,
-                  borderRadius: BorderRadius.circular(8.0),
-                  border: Border.all(
-                    color: Colors.white,
-                    width: 1.0,
-                  ),
+      backgroundColor: Colors.transparent,
+      body: Container(
+        decoration: _showShimmer
+            ? BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    Colors.grey[300]!,
+                    Colors.grey[100]!,
+                  ],
+                  begin: Alignment.centerLeft,
+                  end: Alignment.centerRight,
                 ),
-                child: const SizedBox(
-                  width: 80,
-                  height: 100,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(
-                        Icons.add,
-                        size: 40,
-                        color: Colors.white,
-                      ),
-                      Text(
-                        "Neues Buch hinzufügen",
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          color: Colors.white,
+              )
+            : CustomTheme.getBackgroundGradient(),
+        child: Column(
+          children: [
+            AppBar(
+              title: const Text(
+                "Wunschliste",
+                style: TextStyle(
+                  fontFamily: 'DancingScript',
+                  fontWeight: FontWeight.bold,
+                  fontSize: 24,
+                  color: Colors.white,
+                ),
+              ),
+              leading: IconButton(
+                icon: const Icon(Icons.arrow_back),
+                color: Colors.white,
+                onPressed: () {
+                  // Zurücknavigieren
+                },
+              ),
+              actions: [
+                myCircularAvatar(),
+                const SizedBox(width: 16),
+              ],
+              backgroundColor: Colors.transparent,
+              elevation: 0,
+            ),
+            Expanded(
+              child: GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                  childAspectRatio: 80 / 100,
+                ),
+                itemCount: wishList.length + 1,
+                itemBuilder: (context, index) {
+                  if (index == 0) {
+                    return GestureDetector(
+                      onTap: addNewBook,
+                      child: Container(
+                        margin: const EdgeInsets.all(8),
+                        decoration: BoxDecoration(
+                          color: CustomTheme.loginGradientStart,
+                          borderRadius: BorderRadius.circular(8.0),
+                          border: Border.all(
+                            color: Colors.white,
+                            width: 1.0,
+                          ),
+                        ),
+                        child: const SizedBox(
+                          width: 80,
+                          height: 100,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(
+                                Icons.add,
+                                size: 40,
+                                color: Colors.white,
+                              ),
+                              Text(
+                                "Neues Buch hinzufügen",
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ],
+                          ),
                         ),
                       ),
-                    ],
-                  ),
-                ),
+                    );
+                  } else {
+                    final bookKey = wishList.keys.elementAt(index - 1);
+                    return GestureDetector(
+                      onTap: () {
+                        // Navigator.push
+                      },
+                      child: Container(
+                        margin: const EdgeInsets.all(8),
+                        child: SizedBox(
+                          width: 80,
+                          height: 100,
+                          child: _showShimmer
+                              ? Shimmer.fromColors(
+                                  baseColor: Colors.transparent,
+                                  highlightColor: Colors.grey[100]!,
+                                  child: Container(
+                                    width: 80,
+                                    height: 100,
+                                    color: CustomTheme.darkRed,
+                                  ),
+                                )
+                              : Image.asset(
+                                  wishList[bookKey]!['image']!,
+                                  fit: BoxFit.cover,
+                                ),
+                        ),
+                      ),
+                    );
+                  }
+                },
               ),
-            );
-          } else {
-            final bookKey = wishList.keys.elementAt(index - 1);
-            return GestureDetector(
-              onTap: () {
-                // Navigator.push
-              },
-              child: Container(
-                margin: const EdgeInsets.all(8),
-                child: SizedBox(
-                  width: 80,
-                  height: 100,
-                  child: Image.asset(
-                    wishList[bookKey]!['image']!,
-                    fit: BoxFit.cover,
-                  ),
-                ),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: CurvedNavigationBar(
+        index: 1,
+        backgroundColor: CustomTheme.loginGradientStart,
+        color: CustomTheme.darkMode,
+        buttonBackgroundColor: CustomTheme.darkMode,
+        onTap: (int index) {
+          if (index == 0) {
+            Navigator.of(context).pushAndRemoveUntil(
+              MaterialPageRoute(
+                builder: (context) => const HomeScreen(),
               ),
+              (route) => false,
             );
           }
         },
+        items: const <Widget>[
+          Icon(Icons.home, size: 30, color: Colors.white),
+          Icon(Icons.menu_book, size: 30, color: Colors.white),
+          Icon(Icons.settings, size: 30, color: Colors.white),
+          Icon(Icons.help_outline, size: 30, color: Colors.white),
+        ],
       ),
     );
   }
