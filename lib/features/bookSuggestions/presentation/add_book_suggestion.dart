7360db42a1aabe0325f9b5bf8474e07326776423
Queengirl1990 <bruehlanja90@gmail.com>
2024-02-05 11:49:20 +0100

    add book design

--- lib/features/bookSuggestions/presentation/add_book_suggestion.dart
+++ lib/features/bookSuggestions/presentation/add_book_suggestion.dart
@@ -1,39 +1,19 @@
 import 'package:bookieslist/backend/styles/theme.dart';
 import 'package:bookieslist/backend/widgets/bookieslist_widgets.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
-import 'dart:async';
 
 import '../../../backend/styles/appbar.dart';
-
-//mehr laden button entfernt da er fehler geworfen hat, nochmal überarbeiten
-//Appbar noch darkred
+import '../../home/presentation/home_screen.dart';
 
 void main() {
   runApp(
-    ProviderScope(
-      child: MyApp(),
+    MaterialApp(
+      home: NewBookAdd(),
+      theme: CustomTheme.getAppTheme(),
     ),
   );
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: NewBookAdd(),
-      theme: ThemeData.light().copyWith(
-        primaryColor: CustomTheme.darkRed,
-        scaffoldBackgroundColor: Colors.transparent,
-        appBarTheme: const AppBarTheme(
-          backgroundColor: CustomTheme.darkRed, // Änderung hier
-          elevation: 0,
-        ),
-      ),
-    );
-  }
 }
 
 class NewBookAdd extends StatefulWidget {
@@ -42,116 +22,129 @@ class NewBookAdd extends StatefulWidget {
 }
 
 class _NewBookAddState extends State<NewBookAdd> {
-  TextEditingController _searchController = TextEditingController();
+  final TextEditingController _searchController = TextEditingController();
   List<BookInfo> _searchResults = [];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: CustomTheme.darkRed,
-        elevation: 0,
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back, color: CustomTheme.snowWhite),
-          onPressed: () {
-            Navigator.pop(context);
-          },
-        ),
-        actions: [
-          myCircularAvatar(),
-        ],
-      ),
-      body: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: <Color>[
-              CustomTheme.loginGradientStart,
-              CustomTheme.loginGradientEnd,
-            ],
-            begin: FractionalOffset(0.0, 0.0),
-            end: FractionalOffset(1.0, 1.0),
-            stops: <double>[0.0, 1.0],
-            tileMode: TileMode.clamp,
+      body: Stack(
+        children: [
+          Container(
+            decoration: CustomTheme.getBackgroundGradient(),
           ),
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            const SizedBox(height: 20),
-            const Text(
-              "Neues Buch hinzufügen",
-              style: TextStyle(
-                fontFamily: 'DancingScript',
-                fontWeight: FontWeight.normal,
-                fontSize: 24,
-                color: CustomTheme.snowWhite,
-              ),
-            ),
-            const SizedBox(height: 20),
-            const MyDividerWithIcons(),
-            const SizedBox(height: 20),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: TextField(
-                      controller: _searchController,
-                      style: const TextStyle(color: CustomTheme.snowWhite),
-                      decoration: InputDecoration(
-                        hintText: 'Suche nach Büchern...',
-                        hintStyle:
-                            const TextStyle(color: CustomTheme.snowWhite),
-                        filled: true,
-                        fillColor: CustomTheme.snowWhite.withOpacity(0.2),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
-                          borderSide: BorderSide.none,
-                        ),
-                        contentPadding: const EdgeInsets.symmetric(
-                            vertical: 10, horizontal: 15),
-                      ),
-                      onChanged: (value) {
-                        // Eingabe verarbeiten
-                      },
-                      onSubmitted: (value) {
-                        _searchBooks();
+          Column(
+            children: [
+              SizedBox(
+                  height: MediaQuery.of(context)
+                      .padding
+                      .top), // Adjust for status bar
+              Container(
+                height: 56.0, // AppBar height
+                padding: EdgeInsets.symmetric(horizontal: 16.0),
+                color: Colors.transparent,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    IconButton(
+                      icon: const Icon(Icons.arrow_back,
+                          color: CustomTheme.snowWhite),
+                      onPressed: () {
+                        Navigator.pop(context);
                       },
                     ),
-                  ),
-                  IconButton(
-                    icon: const Icon(Icons.send, color: CustomTheme.snowWhite),
-                    onPressed: () {
-                      // Suche starten
-                      _searchBooks();
-                    },
-                  ),
-                ],
+                    myCircularAvatar(),
+                  ],
+                ),
               ),
-            ),
-            const SizedBox(height: 20),
-            Expanded(
-              child: ListView.builder(
-                itemCount: _searchResults.length,
-                itemBuilder: (context, index) {
-                  final BookInfo result = _searchResults[index];
-                  return ListTile(
-                    leading: result.thumbnailUrl != null
-                        ? Image.network(
-                            result.thumbnailUrl!,
-                            width: 50,
-                            height: 50,
-                            fit: BoxFit.cover,
-                          )
-                        : const SizedBox.shrink(),
-                    title: Text(result.title,
-                        style: const TextStyle(color: CustomTheme.snowWhite)),
-                  );
-                },
+              Expanded(
+                child: SingleChildScrollView(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      const SizedBox(height: 20),
+                      const Text(
+                        "Neues Buch hinzufügen",
+                        style: TextStyle(
+                          fontFamily: 'DancingScript',
+                          fontWeight: FontWeight.normal,
+                          fontSize: 24,
+                          color: CustomTheme.snowWhite,
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      const MyDividerWithIcons(),
+                      const SizedBox(height: 20),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 16),
+                        child: Row(
+                          children: [
+                            Expanded(
+                              child: TextField(
+                                controller: _searchController,
+                                style: const TextStyle(
+                                    color: CustomTheme.snowWhite),
+                                decoration: InputDecoration(
+                                  hintText: 'Suche nach Büchern...',
+                                  hintStyle: const TextStyle(
+                                      color: CustomTheme.snowWhite),
+                                  filled: true,
+                                  fillColor:
+                                      CustomTheme.snowWhite.withOpacity(0.2),
+                                  border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(10),
+                                    borderSide: BorderSide.none,
+                                  ),
+                                  contentPadding: const EdgeInsets.symmetric(
+                                      vertical: 10, horizontal: 15),
+                                ),
+                                onChanged: (value) {
+                                  // Eingabe verarbeiten
+                                },
+                                onSubmitted: (value) {
+                                  _searchBooks();
+                                },
+                              ),
+                            ),
+                            IconButton(
+                              icon: const Icon(Icons.send,
+                                  color: CustomTheme.snowWhite),
+                              onPressed: () {
+                                _searchBooks();
+                              },
+                            ),
+                          ],
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      ListView.builder(
+                        shrinkWrap: true,
+                        physics: NeverScrollableScrollPhysics(),
+                        itemCount: _searchResults.length,
+                        itemBuilder: (context, index) {
+                          final BookInfo result = _searchResults[index];
+                          return ListTile(
+                            leading: result.thumbnailUrl != null
+                                ? Image.network(
+                                    result.thumbnailUrl!,
+                                    width: 50,
+                                    height: 50,
+                                    fit: BoxFit.cover,
+                                  )
+                                : const SizedBox.shrink(),
+                            title: Text(result.title,
+                                style: const TextStyle(
+                                    color: CustomTheme.snowWhite)),
+                          );
+                        },
+                      ),
+                    ],
+                  ),
+                ),
               ),
-            ),
-          ],
-        ),
+            ],
+          ),
+        ],
       ),
     );
   }
