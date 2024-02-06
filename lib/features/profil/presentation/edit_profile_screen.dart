17427a84aac33aec562e37a060400fa53847240b
Queengirl1990 <bruehlanja90@gmail.com>
2024-02-06 14:36:35 +0100

    edit navbar

--- lib/features/profil/presentation/edit_profile_screen.dart
+++ lib/features/profil/presentation/edit_profile_screen.dart
@@ -1,74 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import '../../../backend/styles/theme.dart';
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData.light().copyWith(
-        primaryColor: CustomTheme.darkRed,
-        scaffoldBackgroundColor: Colors.transparent,
-        appBarTheme: const AppBarTheme(
-          backgroundColor: Colors.transparent,
-          elevation: 0,
-        ),
-      ),
-      home: Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            colors: <Color>[
-              CustomTheme.loginGradientStart,
-              CustomTheme.loginGradientEnd,
-            ],
-            begin: FractionalOffset(0.0, 0.0),
-            end: FractionalOffset(1.0, 1.0),
-            stops: <double>[0.0, 1.0],
-            tileMode: TileMode.clamp,
-          ),
-        ),
-        child: const EditProfilScreen(),
-      ),
-    );
-  }
-}
-
-class MyCircularAvatar extends StatelessWidget {
-  const MyCircularAvatar({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: 100,
-      height: 100,
-      decoration: BoxDecoration(
-        color: CustomTheme.snowWhite,
-        shape: BoxShape.circle,
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black.withOpacity(0.5),
-            spreadRadius: 2,
-            blurRadius: 5,
-            offset: const Offset(0, 0),
-          ),
-        ],
-      ),
-      child: ClipOval(
-        child: Image.asset(
-          'assets/images/avatar.png',
-          width: 100,
-          height: 100,
-          fit: BoxFit.cover,
-        ),
-      ),
-    );
-  }
-}
-
-class EditProfilScreen extends StatelessWidget {
+class EditProfilScreen extends StatefulWidget {
   const EditProfilScreen({Key? key}) : super(key: key);
+
+  @override
+  _EditProfilScreenState createState() => _EditProfilScreenState();
+}
+
+class _EditProfilScreenState extends State<EditProfilScreen> {
+  int currentPageIndex = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -160,6 +103,41 @@ class EditProfilScreen extends StatelessWidget {
           ),
         ),
       ),
+      bottomNavigationBar: CurvedNavigationBar(
+        index: currentPageIndex,
+        backgroundColor: Colors.transparent,
+        color: CustomTheme.darkMode,
+        buttonBackgroundColor: CustomTheme.darkMode,
+        onTap: (int index) {
+          setState(() {
+            currentPageIndex = index;
+            switch (index) {
+              case 0:
+                // Home Icon - Zu HomeScreen navigieren
+                Navigator.pushNamed(context, '/');
+                break;
+              case 1:
+                // Menu Book - Zu UnreadBooks navigieren
+                Navigator.pushNamed(context, '/unreadBooks');
+                break;
+              case 2:
+                // Settings - Zum SettingsScreen navigieren
+                Navigator.pushNamed(context, '/settings');
+                break;
+              case 3:
+                // Help Outline - Zum RandomGeneratorScreen navigieren
+                Navigator.pushNamed(context, '/randomGenerator');
+                break;
+            }
+          });
+        },
+        items: const <Widget>[
+          Icon(Icons.home, size: 30, color: Colors.white),
+          Icon(Icons.menu_book, size: 30, color: Colors.white),
+          Icon(Icons.settings, size: 30, color: Colors.white),
+          Icon(Icons.help_outline, size: 30, color: Colors.white),
+        ],
+      ),
     );
   }
 
@@ -170,5 +148,37 @@ class EditProfilScreen extends StatelessWidget {
     if (pickedFile != null) {
       // Logik
     }
+  }
+}
+
+class MyCircularAvatar extends StatelessWidget {
+  const MyCircularAvatar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 100,
+      height: 100,
+      decoration: BoxDecoration(
+        color: CustomTheme.snowWhite,
+        shape: BoxShape.circle,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.5),
+            spreadRadius: 2,
+            blurRadius: 5,
+            offset: const Offset(0, 0),
+          ),
+        ],
+      ),
+      child: ClipOval(
+        child: Image.asset(
+          'assets/images/avatar.png',
+          width: 100,
+          height: 100,
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
   }
 }
